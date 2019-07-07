93965511a7ff4d61931e2676b672e6a64f5a6a4c
Wang Zi-yi <zainking@foxmail.com>
2019-07-07 23:47:53 +0800

    Finish filter (#6)
    Finish filter (#6)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -3,6 +3,7 @@ package com.github.hcsp.polymorphism;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class User {
     /** 用户ID，数据库主键，全局唯一 */
@@ -26,38 +27,22 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users, user -> user.id % 2 == 0);
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users, user -> user.name.startsWith("张"));
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("王")) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users, user -> user.name.startsWith("王"));
     }
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+        return users.stream().filter(predicate).collect(Collectors.toList());
+    }
 }
