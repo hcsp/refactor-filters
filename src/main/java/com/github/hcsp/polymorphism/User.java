80f984822fe97bd13a763758d330d3e7ceb7882f
东方来客 <39690418+westee@users.noreply.github.com>
2019-07-12 22:45:58 +0800

    yes-or-not (#10)
    yes-or-not (#10)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -24,40 +25,91 @@ public class User {
         return name;
     }
 
-    // 过滤ID为偶数的用户
-    public static List<User> filterUsersWithEvenId(List<User> users) {
+    public static List<User> filter (List<User> users, Predicate <User> predicate){
         List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
+        for (User user: users) {
+            if(predicate.test(user)){
                 results.add(user);
             }
         }
         return results;
+    }
+
+    public static void main(String[] args) {
+        filterUsersWithEvenId(Arrays.asList(
+                new User(1,"a"), new User(2,"b")
+        ));
+    }
+
+//    private  interface  判断条件是否成立 {
+//         boolean 这个用户是否满足条件(User user);
+//    }
+
+//    private static class 判断用户id是否为偶数 implements 判断条件是否成立{
+//
+//        @Override
+//        public boolean 这个用户是否满足条件(User user) {
+//            if(user.id % 2 == 0){
+//                return true;
+//            }
+//            return false;
+//        }
+//    }
+
+    // 过滤ID为偶数的用户
+    public static List<User> filterUsersWithEvenId(List<User> users) {
+        return filter(users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.id % 2 == 0;
+            }
+        });
+//        List<User> results = new ArrayList<>();
+//        for (User user : users) {
+//            if (user.id % 2 == 0) {
+//                results.add(user);
+//            }
+//        }
+//        return results;
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
+        return filter(users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.name.startsWith("张");
             }
-        }
-        return results;
+        });
+//        List<User> results = new ArrayList<>();
+//        for (User user : users) {
+//            if (user.name.startsWith("张")) {
+//                results.add(user);
+//            }
+//        }
+//        return results;
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("王")) {
-                results.add(user);
+        return filter(users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.name.startsWith("王");
             }
-        }
-        return results;
+        });
+//        List<User> results = new ArrayList<>();
+//        for (User user : users) {
+//            if (user.name.startsWith("王")) {
+//                results.add(user);
+//            }
+//        }
+//        return results;
     }
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
+//    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
+
+
 }
