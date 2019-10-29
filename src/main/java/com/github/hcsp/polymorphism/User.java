cad465cfba2a4a8aeaaa0387be30e730a4f5cf19
emm <3108775859@qq.com>
2019-10-29 16:08:16 +0800

    Predicate接口和lambda表达式

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -26,38 +26,39 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
+        return filter(users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.id %2 == 0;
             }
-        }
-        return results;
+        });
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
-            }
-        }
-        return results;
+        return filter(users , user -> user.name.startsWith("张"));
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
+            return filter(users, new Predicate<User>() {
+                @Override
+                public boolean test(User user) {
+                    return user.name.startsWith("王");
+                }
+            });
+        }
+
+    // 你可以发现，在上面三个函数中包含大量的重复代码。
+    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
+    // 并简化上面三个函数
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("王")) {
+        for (User user:users){
+            if (predicate.test(user)){
                 results.add(user);
             }
         }
         return results;
     }
-    // 你可以发现，在上面三个函数中包含大量的重复代码。
-    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
-    // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
 }
