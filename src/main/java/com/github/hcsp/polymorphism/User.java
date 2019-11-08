e47e5239702edcad012dda2a08be74036d22edd3
DoubleP <pengp@corp.21cn.com>
2019-11-08 18:14:44 +0800

    抽出方法呀 Modify it third

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -38,12 +38,13 @@ public class User {
     public static List<User> filterWangUsers(List<User> users) {
        return filter(users,user -> user.getName().startsWith("王"));
     }
+
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
     public static List<User> filter(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
-        for (User user : results) {
+        for (User user : users) {
             if (predicate.test(user)) {
                 results.add(user);
             }
