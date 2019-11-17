c5bb66035cf2220061348b11aa29ef5a7a459bcb
Kelvin Chen <wheelchen97@gmail.com>
2019-11-17 16:27:56 +0800

    Stream 方法 (#66)
    Stream 方法 (#66)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -3,12 +3,17 @@ package com.github.hcsp.polymorphism;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -26,38 +31,38 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
-            }
-        }
-        return results;
+        Predicate<User> odds = user -> user.id % 2 == 0;
+        return filter(users, odds);
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
+        Predicate<User> startWith = user -> user.name.startsWith("张");
+        return filter(users, startWith);
+    }
+
+    // 过滤姓王的用户
+    public static List<User> filterWangUsers(List<User> users) {
+        Predicate<User> startWith = user -> user.name.startsWith("王");
+        return filter(users, startWith);
+    }
+
+    // 你可以发现，在上面三个函数中包含大量的重复代码。
+    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
+    // 并简化上面三个函数
+    public static List<User> filter2(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
         for (User user : users) {
-            if (user.name.startsWith("张")) {
+            if (predicate.test(user)) {
                 results.add(user);
             }
         }
         return results;
     }
 
-    // 过滤姓王的用户
-    public static List<User> filterWangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("王")) {
-                results.add(user);
-            }
-        }
-        return results;
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+        return users.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
-    // 你可以发现，在上面三个函数中包含大量的重复代码。
-    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
-    // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
 }
