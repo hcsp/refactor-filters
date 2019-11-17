8dce2ac39d70942562621aa61b4ee8a97631c2b1
wheelchen <15088558209@163.com>
2019-11-17 16:25:50 +0800

    Stream 方法

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -3,12 +3,17 @@ package com.github.hcsp.polymorphism;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -23,6 +28,7 @@ public class User {
     public String getName() {
         return name;
     }
+
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
         Predicate<User> odds = user -> user.id % 2 == 0;
@@ -40,10 +46,11 @@ public class User {
         Predicate<User> startWith = user -> user.name.startsWith("王");
         return filter(users, startWith);
     }
+
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+    public static List<User> filter2(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
         for (User user : users) {
             if (predicate.test(user)) {
@@ -51,5 +58,11 @@ public class User {
             }
         }
         return results;
+    }
+
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+        return users.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }
