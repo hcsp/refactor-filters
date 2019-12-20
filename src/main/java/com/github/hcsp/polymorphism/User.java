934672db20d25212d1e8547fa46964cad7aa9dbf
sunp <sunp@SH-CF12-DEV.rnd.aspentech.com>
2019-12-20 00:48:07 -0800

    Java面向对象初步练习：使用接口抽取过滤器函数

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class User {
+public class User{
     /** 用户ID，数据库主键，全局唯一 */
     private final Integer id;
 
@@ -24,40 +24,47 @@ public class User {
         return name;
     }
 
-    // 过滤ID为偶数的用户
-    public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
-            }
-        }
-        return results;
-    }
+	/*
+	 *  过滤ID为偶数的用户
+	 */    
+    Predicate<User> filterUsersWithEvenId = new Predicate<User>() {
+    	@Override 
+    	public boolean test(User t) {
+    		return t.id % 2 == 0;
+    	}
+    };
 
-    // 过滤姓张的用户
-    public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
-            }
-        }
-        return results;
-    }
+	/*
+	 * 过滤姓张的用户
+	 */    
+    Predicate<User> filterZhangUsers = new Predicate<User>() {
+    	@Override 
+    	public boolean test(User t) {
+    		return t.name.startsWith("张");
+    	}
+    };
 
-    // 过滤姓王的用户
-    public static List<User> filterWangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
+	/*
+	 *  过滤姓王的用户
+	 */    
+    Predicate<User> filterWangUsers = new Predicate<User>() {
+    	@Override 
+    	public boolean test(User t) {
+    		return t.name.startsWith("王");
+    	}
+    };
+    
+	/*
+	 *  你可以发现，在上面三个函数中包含大量的重复代码。 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
+	 * 并简化上面三个函数
+	 */    
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+    	List<User> results = new ArrayList<>();
         for (User user : users) {
-            if (user.name.startsWith("王")) {
+            if (predicate.test(user)) {
                 results.add(user);
             }
         }
         return results;
     }
-    // 你可以发现，在上面三个函数中包含大量的重复代码。
-    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
-    // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
 }
