1068a35a5ee8e74758cfb747f540859a0c47188d
M40 <1149752574@qq.com>
2020-03-31 08:35:42 +0800

    Java面向对象初步练习：使用接口抽取过滤器函数

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,15 +1,18 @@
 package com.github.hcsp.polymorphism;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -27,33 +30,19 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        return filter(users, new Predicate<User>() {
-            @Override
-            public boolean test(User user) {
-                return user.id % 2 == 0;
-            }
-        });
+        return filter(users, user -> user.id % 2 == 0);
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        return filter(users, new Predicate<User>() {
-            @Override
-            public boolean test(User user) {
-                return user.name.startsWith("张") ;
-            }
-        });
+        return filter(users, user -> user.name.startsWith("张"));
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
-        return filter(users, new Predicate<User>() {
-            @Override
-            public boolean test(User user) {
-                return user.name.startsWith("王") ;
-            }
-        });
+        return filter(users, user -> user.name.startsWith("王"));
     }
+
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
@@ -64,5 +53,6 @@ public class User {
                 results.add(user);
             }
         }
+        return results;
     }
 }
