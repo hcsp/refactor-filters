61bd86f87cb4215b00bf013783927ebf3fb4f59a
qpWei <42760426+qipengwei@users.noreply.github.com>
2020-11-24 22:44:03 +0800

    使用predicate抽象公共方法 (#179)
    使用predicate抽象公共方法 (#179)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -26,38 +27,45 @@ public class User {
 
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.id % 2 == 0) {
-                results.add(user);
+        return filter( users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.id % 2 == 0;
             }
-        }
-        return results;
+        } );
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("张")) {
-                results.add(user);
+        return filter( users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.name.startsWith( "张" );
             }
-        }
-        return results;
+        } );
     }
 
     // 过滤姓王的用户
     public static List<User> filterWangUsers(List<User> users) {
-        List<User> results = new ArrayList<>();
-        for (User user : users) {
-            if (user.name.startsWith("王")) {
-                results.add(user);
+        //在使用时传入匿名内部类 以及实现条件表达式
+        return filter( users, new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.name.startsWith( "王" );
             }
-        }
-        return results;
+        } );
     }
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate<User> predicate) {}
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
+        List<User> result = new ArrayList<>();
+        //函数式方法接口提供test表达式判断方法 外部调用方法可直接使用函数表达式入参
+        users.forEach( user -> {
+            if(predicate.test( user )) {
+                result.add( user );
+            }
+        } );
+        return result;
+    }
 }
