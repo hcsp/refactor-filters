0d610466352dfd6650538722f750fb2cb0f8cbe4
“idpeng” <630606938@qq.com>
2021-08-31 13:56:36 +0800

    通过Predicate接口将上述代码抽取成一个公用的过滤器函数

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -88,7 +88,5 @@ public class User {
     // 你可以发现，在上面三个函数中包含大量的重复代码。
     // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-//    public static List<User> filter(List<User> users, Predicate<User> predicate) {
-//
-//    }
+
 }
