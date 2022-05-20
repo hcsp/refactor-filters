8e68c6e6ff0fff46154c72e412d19b014a3d37ac
DDB324 <jiang128193895>
2022-05-20 16:42:38 +0800

    恢复test里面的代码，之前测试的时候不小心注释了

--- src/test/java/com/github/hcsp/polymorphism/UserTest.java
+++ src/test/java/com/github/hcsp/polymorphism/UserTest.java
@@ -9,19 +9,19 @@ import org.junit.jupiter.api.Test;
 public class UserTest {
     @Test
     public void compareToMethodIsCorrect() {
-//        List<User> users =
-//                Arrays.asList(
-//                        new User(1, "a"), new User(2, "b"), new User(3, "c"), new User(4, "d"));
-//
-//        Assertions.assertEquals(
-//                Arrays.asList("a"),
-//                User.filter(users, u -> u.getId() == 1).stream()
-//                        .map(User::getName)
-//                        .collect(Collectors.toList()));
-//        Assertions.assertEquals(
-//                Arrays.asList("b", "d"),
-//                User.filter(users, u -> u.getId() % 2 == 0).stream()
-//                        .map(User::getName)
-//                        .collect(Collectors.toList()));
+        List<User> users =
+                Arrays.asList(
+                        new User(1, "a"), new User(2, "b"), new User(3, "c"), new User(4, "d"));
+
+        Assertions.assertEquals(
+                Arrays.asList("a"),
+                User.filter(users, u -> u.getId() == 1).stream()
+                        .map(User::getName)
+                        .collect(Collectors.toList()));
+        Assertions.assertEquals(
+                Arrays.asList("b", "d"),
+                User.filter(users, u -> u.getId() % 2 == 0).stream()
+                        .map(User::getName)
+                        .collect(Collectors.toList()));
     }
 }
